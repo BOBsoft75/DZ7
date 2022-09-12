43a1732bd381e84fea6a3469c9259c97f73efc71
BOBsoft75 <bobsoft_spb@mail.ru>
2022-09-12 14:38:58 +0300

    DZ7

---
+++ DZ7_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Write("Введите номер строки элемента: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца элемента: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[10, 10];
+
+if (x < 10 && y < 10)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Random rand = new Random();
+            matrix[i, j] = Math.Round((rand.NextDouble() * new Random().Next(-9, 10)), 1);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine(" -> "+matrix[x-1,y-1]);
+}
+else 
+{
+    Console.WriteLine("Такого числа в массиве нет");
+}
---
+++ DZ7_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите число строк массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[x, y];
+
+Console.WriteLine("В массиве случайных целых чисел: ");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+
+for (int i = 0; i < matrix.GetLength(1); i++)
+{
+    double columnSum = 0;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        columnSum = columnSum + matrix[j, i];
+    }
+    Console.Write(Math.Round((columnSum / (y)), 1) + "\t");
+}
